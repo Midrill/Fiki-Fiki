7c6559410e76046b8029bc83451e3d058d643191
Rasty90 <rasty1995@hotmail.it>
2017-06-02 11:11:40 +0200

    Rozza implementazione della GUI con menuscene composto da un layout
    Il layout e buggato siccome non si allinea correttamente

--- GUI/src/TestGUI.java
+++ GUI/src/TestGUI.java
@@ -1,10 +1,14 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Ellipse;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -20,20 +24,56 @@ public class TestGUI extends Application {
 
     @Override
     public void start(Stage stage){
-        Rectangle r = new Rectangle(40, 20, 200, 80);
-        // Rettangolo in (40, 20)
-        Ellipse e = new Ellipse(120, 70, 80, 60);
-        // Ellisse con centro (120, 70)
-        e.setFill(Color.rgb(0, 51, 153));
-        Group root = new Group(r, e);
-        Scene scene = new Scene(root, 400, 200);
-        // La radice del grafo di scena
-        scene.setFill(Color.rgb(184, 180, 125));
-        // Colore di background della scena
-        stage.setScene(scene);
-        // La scena della finestra
+
+
+        // creazione della finestra di immissione dati
+        Text t1 = new Text("Fiki-Fiki Simulator");
+        VBox vb = new VBox(t1);
+        vb.setAlignment(Pos.TOP_LEFT);
+        Text textm = new Text("Numero Morigerati");
+        Text texta = new Text("Numero Avventurieri");
+        Text textp = new Text("Numero Prudenti");
+        Text texts = new Text("Numero Spregiudicate");
+        HBox hb = new HBox(textm, texta, textp, texts);
+        hb.setAlignment(Pos.BOTTOM_CENTER);
+        hb.setSpacing(50);
+
+        // creazione della finestra di simulazione
+        Rectangle graphic = new Rectangle(0, 0, 600, 400);
+        graphic.setFill(Color.rgb(0, 83, 255));
+        // allocazione di area in alto sx per disegnare i grafici
+
+        Rectangle chart = new Rectangle(0, 400, 600, 200);
+        chart.setFill(Color.rgb(255, 34, 54));
+        // allocazione di area per i grafici a torta
+
+        Rectangle data = new Rectangle(600, 0, 200, 600);
+        data.setFill(Color.grayRgb(0));
+        // allocazione di area per i print delle variazioni nello stadio
+
+        Group simulation = new Group(graphic, chart, data); // gruppo di oggetti di simulazione
+        Group menu = new Group(vb, hb); // gruppo di oggetti del menu iniziale
+
+        Scene simulationscene = new Scene(simulation, 800, 600);
+        simulationscene.setFill(Color.grayRgb(255));
+        // creazione di scena contenente oggetti simulazione
+
+        Scene menuscene = new Scene(menu, 800, 600);
+        menuscene.setFill(Color.grayRgb(255));
+        // creazione di scena contenente oggetti menu
+
+        stage.setScene(menuscene);
+        // inizialmente verra mostrata la finestra di menu per immissione dati
+
         stage.show();
         // Rende visibile la finestra (o stage) principale
+
+
+
+
+
+
+
 
     }
 
